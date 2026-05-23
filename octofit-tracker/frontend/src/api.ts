9c339978a98271bcdce7360cb09c0fbe6bb34e7c
Hiep Nguyen <peih12@gmail.com>
2026-05-23 12:23:32 +0000

    test

--- octofit-tracker/frontend/src/api.ts
+++ octofit-tracker/frontend/src/api.ts
@@ -1,5 +1,5 @@
 const codespaceName = import.meta.env.VITE_CODESPACE_NAME;
-const DEFAULT_API_HOST = "http://localhost:8000";
+const DEFAULT_API_HOST = "https://bug-free-goggles-77vv9pjvgj3r676-8000.app.github.dev";
 
 export const apiHost = codespaceName
   ? `https://${codespaceName}-8000.app.github.dev`
